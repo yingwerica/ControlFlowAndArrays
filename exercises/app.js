8054690b8c581645539a4686637f57ca372987ce
Ying Wang <yingw.pro@gmail.com>
2022-10-12 15:34:25 -0600

    added if/else excercise

--- exercises/app.js
+++ exercises/app.js
@@ -1,14 +1,32 @@
 
 
-let num;
-if (num > 0) {
-    console.log("positive") ;
+let num = 67;
+if (num > 0 && num > 100) {
+    console.log(" The number is larger than 100") ;
 }
-else if (num < 0) {
-console.log ("negative");
+else if (num > 0 && num < 100) {
+console.log ("The number is between 0 and 100");
 }
-else {
-    console.log ("not negative nor positive");
+else if ( num < 0) {
+    console.log ("The number is negative");
     }
 
-num = -2;
+let grade = 40;
+
+if (grade >= 90) {
+    console.log( "Your grade is A.")
+}
+else if (grade >= 80 && grade < 90) {
+    console.log( "Your grade is B.")
+} 
+else if (grade >= 70 && grade < 80) {
+    console.log( "Your grade is C.")
+} 
+else if (grade >= 55 && grade < 70) {
+    console.log( "Your grade is D.")
+} else {
+    console.log( "Your grade is F.")
+}
+
+
+
