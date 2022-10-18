97dd27e7815ca805c0c8780f0c80e85b2bb6f731
Ying Wang <yingw.pro@gmail.com>
2022-10-18 13:37:46 -0600

    new commit

--- exercises/functions/functions.js
+++ exercises/functions/functions.js
@@ -1,12 +1,52 @@
 //function declaration
-function one() {
+/*function one() {
     return "one"
 }
-console.log(one()); 
+console.log(one()); */
 
 
-const two = () => {
+/*const two = () => {
     return 2;
 }
-console.log(two()); 
+console.log(two()); */
 // const shortenedTwo = () => 2;
+
+
+
+/*function areBothEven(n1, n2) {
+    return !(n1 % 2) && !(n2 % 2);
+}
+console.log(areBothEven(3, 7));
+
+ 
+
+let maria = getDevObject("Maria", "HTML", "CSS", "JavaScript", "jQuery");
+
+function getDevObject(name) {
+  let skills = [];
+  for (let i = 1; i < arguments.length; i++) {
+    skills.push(arguments[i]);
+  }
+  return { devName: name, jobSkills: skills };
+}
+console.log(getDevObject(maria)) */
+
+
+
+
+const areaOfTriangle= (width, height) => {
+    let area= width*height/2;
+    console.log(`The area of a triangle with a width of ${width} and a height of ${height} is ${area} aquare units.`);
+}
+
+areaOfTriangle(3, 4)
+
+const plantHasWater=(planet) => {
+    if (planet ==="Earth" || planet ==="Mars"){
+        return true;
+    } else {
+        return false;
+    }
+    
+} 
+console.log(plantHasWater('Earth'));
